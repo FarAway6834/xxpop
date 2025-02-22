a44ff1d775b74edfbe3ab768273f12e0f0905a5f
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-22 13:31:57 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/FarAway6834/xxpop',
     packages=find_packages(exclude=[]),
     keywords=['xxpop'],
-    python_requires='>=3.0',
+    python_requires='>=2.0',
     package_data={},
     classifiers=[
         'Programming Language :: Python :: 2.0',
